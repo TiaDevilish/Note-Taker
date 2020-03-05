f0d62608028c1bed5c73837da42465e85bc40ee3
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-03-04 22:25:22 -0500

    added the boiler plate

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
