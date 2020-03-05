cd0d504eeb34af533c62bcaf3cdddce00a708d39
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-03-04 22:48:58 -0500

    paths not working

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -7,3 +7,20 @@ const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "./Develop/public/index.html"));
+  });
+  
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+  });
+
+
+
+//at end
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
