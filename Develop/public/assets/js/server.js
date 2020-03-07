5a628e9ff8a46c2f733c41eec38975d8507944b2
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-03-06 19:49:11 -0500

    still getting an error message, but it works

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const allNotes = require("../../../db/db.json")
 
 const app = express();
 const PORT = 7500;
@@ -9,18 +10,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get("/", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + "../../../index.html"));
-    console.log(__dirname)
   });
   
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname + "../../../notes.html"));
   });
 
+  app.get("/api/allnotes", function(req, res){
+    fs.readFile("../../../db/db.json" , "utf8", function(err, data){
+      if(err) throw(err);
+      const notesFile = JSON.parse(data);
+      res.json(notesFile)
+    })
+  });
+
+  app.get("/api/allnotes/:id", function(req, res){
+    let selection = req.params.ID
+    console.log(selection);
+  });
+
+  app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "../../../index.html"));
+  });
+
+
+
+
 
 
 //at end
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log(`App listening on PORT ${PORT}`);
   });
