0889136020cadd2b28fb8d57ae0f0b2ef45448a3
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-03-07 10:14:36 -0500

    moved server js

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const uuid = require("uuid");
 
 const app = express();
 const PORT = 7500;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("assets"));
+app.use(express.static(__dirname + "public/assets"));
 
-var note = [];
+let count = 0;
 
 //routes
   app.get("/", function(req, res) {
@@ -20,27 +19,36 @@ var note = [];
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname + "./../../notes.html"));
   });
-
-//gets
+  
   app.get("/api/notes", function(req, res){
-    return res.json(note);
+    return res.json(notes);
   });
 
+//post
   app.post("/api/notes", function(req, res){
-    var newNote = req.body;
-    newNote.id = uuid();
-    note.push(newNote);
-    res.json(newNote);
-  });
+    if(notes === false)
+    notes = [];
+    let newNote = req.body;
+    count++;
+    let id = count;
+    notes.push(newNote);
+    toStringAndWrite(notes);
+    res.join(notes);
+  })
 
 //deletes
   app.delete("/api/notes/:id", function(req, res){
-    for(i = 0; i < note.length; i++){
-      if(req.params.id == note[i].id){
-        note.splice(i,1)
+    let id = req.params.id;
+    for(let i = 0; i < notes.length; i++){
+      if(notes[i].id== id){
+        notes.splice(i, 1);
+        res.json("deleted")
       }
-      res.json(req.body);
     }
+    if(notes[0] == null)
+      fs.writeFileSync("db/db.json", "[]", "utf-8");
+    else
+    fs.writeFileSync("db/db.json", notes, "utf-8");
   });
 
 //listens
